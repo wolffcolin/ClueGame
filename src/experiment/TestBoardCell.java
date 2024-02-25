841e52e48daea344c032ec7af51d4c1172bc9fbb
wolffcolin <cwcnsa@gmail.com>
2024-02-25 14:53:00 -0700

    added method signatures to TestBoardCell

--- src/experiment/TestBoardCell.java
+++ src/experiment/TestBoardCell.java
@@ -1,5 +1,46 @@
 package experiment;
 
+import java.util.Set;
+
 public class TestBoardCell {
 
+	public TestBoardCell(int setRow, int setColumn) {
+		
+	}
+	
+	void addAdjacency(TestBoardCell cell) {
+		
+	}
+	
+	Set<TestBoardCell> getAdjList() {
+		
+		return null;
+		
+	}
+	
+	void setRoom(boolean isRoom) {
+		
+		
+	}
+	
+	boolean isRoom() {
+	
+		return (Boolean) null;
+		
+	}
+	
+	void setOccupied() {
+		
+		
+		
+	}
+	
+	boolean getOccupied() {
+		
+		return (Boolean) null;
+		
+	}
+	
+	
+	
 }
