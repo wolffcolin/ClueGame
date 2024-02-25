a580f81fb2b5b3dfc861e681b8fe14de07393ed8
eocow <70074743+eocow@users.noreply.github.com>
2024-02-25 14:58:11 -0700

    Added tests to TestBoard

--- src/experiment/TestBoard.java
+++ src/experiment/TestBoard.java
@@ -1,5 +1,24 @@
 package experiment;
 
-public class TestBoard {
+import java.util.*;
 
+public class TestBoard {
+    private ArrayList targets;
+
+    public TestBoard() {
+        super();
+    }
+
+    public void calcTargets(TestBoardCell startCell, int pathLength) {
+    }
+
+    public TestBoardCell getCell(int row, int col) {
+        TestBoardCell cell = new TestBoardCell();
+        return cell;
+    }
+
+    public Set<TestBoardCell> getTargets() {
+        Set<TestBoardCell> set = new HashSet<TestBoardCell>();
+        return set;
+    }
 }
