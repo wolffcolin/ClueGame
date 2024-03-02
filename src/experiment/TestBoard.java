286a3a9554041ed0b0453b4a65aee202de60464f
eocow <70074743+eocow@users.noreply.github.com>
2024-03-01 17:06:21 -0700

    merged changes to calcTargets

--- src/experiment/TestBoard.java
+++ src/experiment/TestBoard.java
@@ -15,88 +15,82 @@ public class TestBoard {
     private Set<TestBoardCell> targets;
     private TestBoardCell[][] grid;
     private Set<TestBoardCell> visited;
-    
+
     final static int COLS = 4;
     final static int ROWS = 4;
 
-    //constructor
+    // constructor
     public TestBoard() {
         super();
-        
+
         this.targets = new HashSet();
         this.visited = new HashSet();
         this.grid = new TestBoardCell[ROWS][COLS];
-        
-        for (int i = 0; i < ROWS ; i++) {
-        	for (int j = 0; j < COLS; j++) {
-        		this.grid[i][j] = new TestBoardCell(i,j);
-        	}
-        }
-        
+
         for (int i = 0; i < ROWS; i++) {
-        	for (int j = 0; j < COLS; j++) {
-        		TestBoardCell cell = this.grid[i][j];
-        		if (i+1 < ROWS) {
-        			if (!grid[i+1][j].isRoom()) {
-        				cell.addAdjacency(grid[i+1][j]);
-        			}
-        		}
-        		if (i -1 > 0) {
-        			if (!grid[i-1][j].isRoom()) {
-        				cell.addAdjacency(grid[i-1][j]);
-        			}
-        		}
-        		if (j + 1 < COLS) {
-        			if (!grid[i][j+1].isRoom()) {
-        				cell.addAdjacency(grid[i][j+1]);
-        			}
-        		}
-        		if (j - 1 > 0) {
-        			if (!grid[i][j-1].isRoom()) {
-        				cell.addAdjacency(grid[i][j-1]);
-        			}
-        		}
-        	}
+            for (int j = 0; j < COLS; j++) {
+                this.grid[i][j] = new TestBoardCell(i, j);
+            }
+        }
+
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < COLS; j++) {
+                TestBoardCell cell = this.grid[i][j];
+                if (i + 1 < ROWS) {
+                    if (!grid[i + 1][j].isRoom()) {
+                        cell.addAdjacency(grid[i + 1][j]);
+                    }
+                }
+                if (i - 1 > 0) {
+                    if (!grid[i - 1][j].isRoom()) {
+                        cell.addAdjacency(grid[i - 1][j]);
+                    }
+                }
+                if (j + 1 < COLS) {
+                    if (!grid[i][j + 1].isRoom()) {
+                        cell.addAdjacency(grid[i][j + 1]);
+                    }
+                }
+                if (j - 1 > 0) {
+                    if (!grid[i][j - 1].isRoom()) {
+                        cell.addAdjacency(grid[i][j - 1]);
+                    }
+                }
+            }
         }
     }
 
-    //calculates the targets for a starting cell
+    // calculates the targets for a starting cell
     public void calcTargets(TestBoardCell startCell, int pathLength) {
-    	
-    	if (pathLength == 0) {
-    		targets.add(startCell);
-    		return;
-    	}
-    	
-    	Set<TestBoardCell> adjCells = startCell.getAdjList();
-    	for (TestBoardCell adj : adjCells) {
-    		
-    		if (!visited.contains(adj) && !adj.getOccupied()) {
-    			
-    			visited.add(adj);
-    			if (pathLength == 1) {
-    				
-    				targets.add(adj);
-    				
-    			} else {
-    				
-    				calcTargets(adj, pathLength - 1);
-    				
-    			}
-    			visited.remove(adj);
-    		}
-    		
-    	}
-    	
+        search(startCell, 0, pathLength);
     }
 
-    //returns the cell at given point
+    private void search(TestBoardCell startCell, int distance, int pathLength) {
+        if (distance > pathLength) {
+            return;
+        }
+
+        Set<TestBoardCell> adjCells = startCell.getAdjList();
+
+        for (TestBoardCell adj : adjCells) {
+
+            if (!visited.contains(adj) && !adj.getOccupied()) {
+
+                visited.add(adj);
+                targets.add(adj);
+                search(adj, distance + 1, pathLength);
+            }
+
+        }
+    }
+
+    // returns the cell at given point
     public TestBoardCell getCell(int row, int col) {
         return grid[row][col];
     }
 
-    //returns target list
+    // returns target list
     public Set<TestBoardCell> getTargets() {
         return targets;
-    }   
+    }
 }
