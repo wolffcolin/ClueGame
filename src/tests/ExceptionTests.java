4321b7aa538fbac474669d4de76159b888b26227
eocow <70074743+eocow@users.noreply.github.com>
2024-03-01 20:15:45 -0700

    added ExceptionTests

--- src/tests/ExceptionTests.java
+++ src/tests/ExceptionTests.java
@@ -1,14 +1,52 @@
 package tests;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.FileNotFoundException;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
+import clueGame.BadConfigFormatException;
+import clueGame.Board;
 
-class ExceptionTests {
+public class ExceptionTests {
 
+	// Test that an exception is thrown for a layout file that does not
+	// have the same number of columns for each row
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testBadColumns() throws BadConfigFormatException, FileNotFoundException {
+		assertThrows(BadConfigFormatException.class, () -> {
+			// Note that we are using a LOCAL Board variable, because each
+			// test will load different files
+			Board board = Board.getInstance();
+			board.setConfigFiles("ClueLayoutBadColumns.csv", "ClueSetup.txt");
+			// Instead of initialize, we call the two load functions directly.
+			// This is necessary because initialize contains a try-catch.
+			board.loadSetupConfig();
+			// This one should throw an exception
+			board.loadLayoutConfig();
+		});
+	}
+
+	// Test that an exception is thrown for a Layout file that specifies
+	// a room that is not in the legend.
+	@Test
+	public void testBadRoom() throws BadConfigFormatException, FileNotFoundException {
+		assertThrows(BadConfigFormatException.class, () -> {
+			Board board = Board.getInstance();
+			board.setConfigFiles("ClueLayoutBadRoom.csv", "ClueSetup.txt");
+			board.loadSetupConfig();
+			board.loadLayoutConfig();
+		});
+	}
+
+	// Test that an exception is thrown for a bad format Setup file
+	@Test
+	public void testBadRoomFormat() throws BadConfigFormatException, FileNotFoundException {
+		assertThrows(BadConfigFormatException.class, () -> {
+			Board board = Board.getInstance();
+			board.setConfigFiles("ClueLayout.csv", "ClueSetupBadFormat.txt");
+			board.loadSetupConfig();
+			board.loadLayoutConfig();
+		});
 	}
 
 }
