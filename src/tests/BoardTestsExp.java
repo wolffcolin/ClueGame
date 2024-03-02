8c06e8a597852983ee92cf8e0a65068fca2a873c
eocow <70074743+eocow@users.noreply.github.com>
2024-03-01 19:27:14 -0700

    commenting to tests

--- src/tests/BoardTestsExp.java
+++ src/tests/BoardTestsExp.java
@@ -21,56 +21,59 @@ import experiment.TestBoardCell;
 import junit.framework.Assert;
 
 class BoardTestsExp {
-	
+
 	TestBoard board;
-	
+
 	@BeforeEach
 	void setup() {
-		
+
 		board = new TestBoard();
-		
+
 	}
 
+	// tests that cells that should be marked adjacnet are marked
 	@SuppressWarnings("deprecation")
 	@Test
 	void testAdjacency() {
-		
+
 		TestBoardCell cell = board.getCell(0, 0);
 		Set<TestBoardCell> testList = cell.getAdjList();
 		Assert.assertTrue(testList.contains(board.getCell(1, 0)));
-		Assert.assertTrue(testList.contains(board.getCell(0, 1)));  
-		
+		Assert.assertTrue(testList.contains(board.getCell(0, 1)));
+
 		TestBoardCell cell1 = board.getCell(0, 3);
 		Set<TestBoardCell> testList1 = cell1.getAdjList();
 		Assert.assertTrue(testList1.contains(board.getCell(0, 2)));
 		Assert.assertTrue(testList1.contains(board.getCell(1, 3)));
-		
+
 		TestBoardCell cell2 = board.getCell(1, 1);
 		Set<TestBoardCell> testList2 = cell2.getAdjList();
 		Assert.assertTrue(testList2.contains(board.getCell(0, 1)));
 		Assert.assertTrue(testList2.contains(board.getCell(1, 0)));
 		Assert.assertTrue(testList2.contains(board.getCell(2, 1)));
 		Assert.assertTrue(testList2.contains(board.getCell(1, 2)));
-		
+
 		TestBoardCell cell3 = board.getCell(2, 2);
 		Set<TestBoardCell> testList3 = cell3.getAdjList();
 		Assert.assertTrue(testList3.contains(board.getCell(2, 1)));
 		Assert.assertTrue(testList3.contains(board.getCell(1, 2)));
 		Assert.assertTrue(testList3.contains(board.getCell(3, 2)));
 		Assert.assertTrue(testList3.contains(board.getCell(2, 3)));
-		
+
 		TestBoardCell cell4 = board.getCell(0, 3);
 		Set<TestBoardCell> testList4 = cell4.getAdjList();
 		Assert.assertTrue(testList4.contains(board.getCell(0, 2)));
 		Assert.assertTrue(testList4.contains(board.getCell(1, 3)));
 	}
-	
+
+	// tests that cells that should be marked as targets are when a cell is marked
+	// as a room
 	@Test
 	void TestTargetsRoom() {
-		
+
 		board.getCell(0, 1).setRoom(true);
 		TestBoardCell cell = board.getCell(1, 3);
-		
+
 		board.calcTargets(cell, 3);
 		Set<TestBoardCell> targets = board.getTargets();
 
@@ -96,18 +99,20 @@ class BoardTestsExp {
 		Assert.assertTrue(targets1.contains(board.getCell(2, 3)));
 		Assert.assertTrue(targets1.contains(board.getCell(1, 2)));
 		Assert.assertTrue(targets1.contains(board.getCell(0, 3)));
-		
+
 	}
-	
+
+	// test that cells that should be marked as targets are when a cell is marked as
+	// occupied
 	@Test
 	void TestTargetsOccupied() {
-		
+
 		TestBoardCell cell = board.getCell(0, 0);
 		board.getCell(3, 3).setOccupied(true);
-		
+
 		board.calcTargets(cell, 3);
 		Set<TestBoardCell> targets = board.getTargets();
-		
+
 		Assert.assertEquals(6, targets.size());
 		Assert.assertTrue(targets.contains(board.getCell(0, 1)));
 		Assert.assertTrue(targets.contains(board.getCell(0, 3)));
@@ -115,15 +120,15 @@ class BoardTestsExp {
 		Assert.assertTrue(targets.contains(board.getCell(1, 2)));
 		Assert.assertTrue(targets.contains(board.getCell(2, 1)));
 		Assert.assertTrue(targets.contains(board.getCell(3, 0)));
-		
+
 		board.getCell(3, 3).setOccupied(false);
-		
+
 		TestBoardCell cell1 = board.getCell(1, 1);
 		board.getCell(2, 2).setOccupied(true);
-		
+
 		board.calcTargets(cell1, 3);
 		Set<TestBoardCell> targets1 = board.getTargets();
-		
+
 		Assert.assertEquals(8, targets.size());
 		Assert.assertTrue(targets1.contains(board.getCell(0, 1)));
 		Assert.assertTrue(targets1.contains(board.getCell(0, 3)));
@@ -133,16 +138,17 @@ class BoardTestsExp {
 		Assert.assertTrue(targets1.contains(board.getCell(2, 3)));
 		Assert.assertTrue(targets1.contains(board.getCell(3, 0)));
 		Assert.assertTrue(targets1.contains(board.getCell(3, 2)));
-		
-		
+
 	}
-	
+
+	// test that cells that should be marked as targets are when a cell is marked as
+	// occupied and as a room
 	@Test
 	void TestTargetsMixed() {
-		
+
 		board.getCell(0, 2).setOccupied(true);
 		board.getCell(1, 2).setRoom(true);
-		
+
 		TestBoardCell cell = board.getCell(0, 3);
 		board.calcTargets(cell, 3);
 		Set<TestBoardCell> targets = board.getTargets();
@@ -150,12 +156,12 @@ class BoardTestsExp {
 		Assert.assertTrue(targets.contains(board.getCell(2, 2)));
 		Assert.assertTrue(targets.contains(board.getCell(1, 1)));
 		Assert.assertTrue(targets.contains(board.getCell(3, 3)));
-		
+
 		board.getCell(0, 2).setOccupied(false);
 		board.getCell(1, 2).setRoom(false);
 		board.getCell(3, 1).setOccupied(true);
 		board.getCell(3, 3).setRoom(true);
-		
+
 		TestBoardCell cell1 = board.getCell(0, 0);
 		board.calcTargets(cell1, 3);
 		Set<TestBoardCell> targets1 = board.getTargets();
@@ -168,7 +174,8 @@ class BoardTestsExp {
 		Assert.assertTrue(targets1.contains(board.getCell(0, 3)));
 	}
 
-	
+	// test that cells that should be marked as targets are when no cells are marked
+	// as occupied or as a room
 	@SuppressWarnings("deprecation")
 	@Test
 	void testTargetsNormal() {
@@ -182,7 +189,7 @@ class BoardTestsExp {
 		Assert.assertTrue(targets.contains(board.getCell(1, 2)));
 		Assert.assertTrue(targets.contains(board.getCell(2, 1)));
 		Assert.assertTrue(targets.contains(board.getCell(3, 0)));
-		
+
 		TestBoardCell cell1 = board.getCell(0, 3);
 		board.calcTargets(cell1, 2);
 		Set<TestBoardCell> targets1 = board.getTargets();
@@ -191,7 +198,6 @@ class BoardTestsExp {
 		Assert.assertTrue(targets1.contains(board.getCell(1, 2)));
 		Assert.assertTrue(targets1.contains(board.getCell(2, 3)));
 
-		
 		TestBoardCell cell2 = board.getCell(3, 2);
 		board.calcTargets(cell2, 1);
 		Set<TestBoardCell> targets2 = board.getTargets();
@@ -199,7 +205,7 @@ class BoardTestsExp {
 		Assert.assertTrue(targets2.contains(board.getCell(3, 1)));
 		Assert.assertTrue(targets2.contains(board.getCell(2, 2)));
 		Assert.assertTrue(targets2.contains(board.getCell(3, 3)));
-		
+
 		TestBoardCell cell3 = board.getCell(2, 3);
 		board.calcTargets(cell3, 4);
 		Set<TestBoardCell> targets3 = board.getTargets();
@@ -212,9 +218,6 @@ class BoardTestsExp {
 		Assert.assertTrue(targets3.contains(board.getCell(3, 0)));
 		Assert.assertTrue(targets3.contains(board.getCell(3, 2)));
 
-
-
-		
 		TestBoardCell cell4 = board.getCell(3, 3);
 		board.calcTargets(cell4, 5);
 		Set<TestBoardCell> targets4 = board.getTargets();
@@ -228,7 +231,6 @@ class BoardTestsExp {
 		Assert.assertTrue(targets4.contains(board.getCell(3, 0)));
 		Assert.assertTrue(targets4.contains(board.getCell(3, 2)));
 
-		
 		TestBoardCell cell5 = board.getCell(3, 0);
 		board.calcTargets(cell5, 2);
 		Set<TestBoardCell> targets5 = board.getTargets();
@@ -236,7 +238,7 @@ class BoardTestsExp {
 		Assert.assertTrue(targets5.contains(board.getCell(1, 0)));
 		Assert.assertTrue(targets5.contains(board.getCell(2, 1)));
 		Assert.assertTrue(targets5.contains(board.getCell(3, 2)));
-		
+
 		TestBoardCell cell6 = board.getCell(1, 1);
 		board.calcTargets(cell6, 4);
 		Set<TestBoardCell> targets6 = board.getTargets();
@@ -249,7 +251,6 @@ class BoardTestsExp {
 		Assert.assertTrue(targets6.contains(board.getCell(3, 1)));
 		Assert.assertTrue(targets6.contains(board.getCell(3, 3)));
 
-		
 		TestBoardCell cell7 = board.getCell(2, 1);
 		board.calcTargets(cell7, 3);
 		Set<TestBoardCell> targets7 = board.getTargets();
@@ -263,5 +264,5 @@ class BoardTestsExp {
 		Assert.assertTrue(targets7.contains(board.getCell(3, 1)));
 		Assert.assertTrue(targets7.contains(board.getCell(3, 3)));
 	}
-	
+
 }
