d448920ab312093d6967813de39cc265bcb58862
wolffcolin <cwcnsa@gmail.com>
2024-03-01 20:30:39 -0700

    message

--- src/clueGame/BoardCell.java
+++ src/clueGame/BoardCell.java
@@ -3,6 +3,8 @@ package clueGame;
 import java.util.HashSet;
 import java.util.Set;
 
+import experiment.TestBoardCell;
+
 public class BoardCell {
     private int row;
     private int col;
@@ -15,6 +17,7 @@ public class BoardCell {
     private boolean roomCenter;
     private char secretPassage;
     private Set<BoardCell> adjList;
+    private boolean occupied;
 
     public BoardCell(int setRow, int setColumn, char type) {
         this.row = setRow;
@@ -42,8 +45,30 @@ public class BoardCell {
         return false;
     }
 
-    public String getName() {
-        return null;
-    }
+	//returns list of adjacent cells
+	public Set<BoardCell> getAdjList() {
+		
+		return this.adjList;
+		
+	}
+	
+	
+	public int getRow() {
+		return this.row;
+	}
+	
+	public int getCol() {
+		return this.col;
+	}
+
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean getOccupied() {
+		// TODO Auto-generated method stub
+		return occupied;
+	}
 
 }
