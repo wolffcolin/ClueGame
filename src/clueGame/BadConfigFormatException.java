160160cf1789b7adf37eded992941ce5a0f8bb98
wolffcolin <cwcnsa@gmail.com>
2024-03-04 11:27:50 -0700

    added functionality to exception to write to log

--- src/clueGame/BadConfigFormatException.java
+++ src/clueGame/BadConfigFormatException.java
@@ -9,12 +9,34 @@ Authors: Colin Wolff and Eoghan Cowley
 
 package clueGame;
 
+import java.util.logging.Logger;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.SimpleFormatter;
+import java.io.IOException;
+
+
 public class BadConfigFormatException extends Exception {
+	
+	private static final Logger LOG = Logger.getLogger(BadConfigFormatException.class.getName());
+	
+	static {
+		try {
+			FileHandler fileHandler = new FileHandler("error.log", true);
+			fileHandler.setFormatter(new SimpleFormatter());
+			LOG.addHandler(fileHandler);
+		} catch (IOException e) {
+			LOG.warning("Failed to access logger handler");
+		}
+	}
+	
     public BadConfigFormatException(String message) {
         super(message);
+        LOG.log(Level.SEVERE, message);
     }
 
     public BadConfigFormatException() {
         super("Bad Config Format in config files encountered");
+        LOG.log(Level.SEVERE, "Bad Config Format in config files encountered");
     }
 }
