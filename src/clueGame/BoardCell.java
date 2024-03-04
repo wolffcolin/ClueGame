77effab3a1b679524256fbb22b4c4d954dc62471
eocow <70074743+eocow@users.noreply.github.com>
2024-03-04 11:25:46 -0700

    refactoring BoardCell

--- src/clueGame/BoardCell.java
+++ src/clueGame/BoardCell.java
@@ -15,38 +15,40 @@ import java.util.Set;
 public class BoardCell {
     private int row;
     private int col;
-
     private char initial;
+    private Set<BoardCell> adjList;
 
     private DoorDirection doorDirection;
-
     private boolean roomLabel;
     private boolean roomCenter;
     private char secretPassage;
-    private Set<BoardCell> adjList;
+
     private boolean occupied;
 
-    // constructor
+    // constructor when there is no second character
     public BoardCell(int setRow, int setColumn, char roomType) {
         this.row = setRow;
         this.col = setColumn;
-
         this.initial = roomType;
+        this.adjList = new HashSet<BoardCell>();
 
         this.roomLabel = false;
         this.roomCenter = false;
-
         this.doorDirection = DoorDirection.NONE;
-
-        this.adjList = new HashSet<BoardCell>();
     }
 
+    // constructor when there is a second character
     public BoardCell(int setRow, int setColumn, char roomType, char secondType) {
         this.row = setRow;
         this.col = setColumn;
         this.initial = roomType;
         this.adjList = new HashSet<BoardCell>();
 
+        /*
+         * checking what the second character type is
+         * character should be tested in board for validity before calling the
+         * constructor
+         */
         if (secondType == '#') {
             this.roomLabel = true;
             this.doorDirection = DoorDirection.NONE;
@@ -61,7 +63,7 @@ public class BoardCell {
             this.doorDirection = DoorDirection.RIGHT;
         } else if (secondType == 'v') {
             this.doorDirection = DoorDirection.DOWN;
-        } else {
+        } else { // must be a secrete passage way
             this.secretPassage = secondType;
             this.doorDirection = DoorDirection.NONE;
         }
@@ -91,35 +93,37 @@ public class BoardCell {
 
     }
 
-    // returns row
+    // returns row position
     public int getRow() {
         return this.row;
     }
 
+    // returns column position
     public int getCol() {
         return this.col;
     }
 
-    public String getName() {
-        return null;
-    }
-
+    // returns bool for occupied or not
     public boolean getOccupied() {
         return this.occupied;
     }
 
+    // returns enum of DoorDirection
     public DoorDirection getDoorDirection() {
         return this.doorDirection;
     }
 
+    // returns bool of if the cell is roomLabel
     public boolean isLabel() {
         return this.roomLabel;
     }
 
+    // returns bool of if the cell is roomCenter
     public boolean isRoomCenter() {
         return this.roomCenter;
     }
 
+    // returns the room code char of where the secrete passage goes to
     public char getSecretPassage() {
         return this.secretPassage;
     }
