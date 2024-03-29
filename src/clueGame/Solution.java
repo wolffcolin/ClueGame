42eb19546ccd8d1170a538cf79d951cd5d81d396
wolffcolin <cwcnsa@gmail.com>
2024-03-28 19:12:07 -0600

    added solution class

---
+++ src/clueGame/Solution.java
@@ -0,0 +1,18 @@
+package clueGame;
+
+public class Solution {
+	private Card room;
+	private Card player;
+	private Card weapon;
+	
+	public Solution(Card room, Card player, Card weapon) {
+		this.room = room;
+		this.player = player;
+		this.weapon = weapon;
+	}
+	
+	public Card[] theAnswer() {
+		Card[] answer = {room, player, weapon};
+		return answer;
+	}
+}
