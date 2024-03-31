513f82518843b6ed799456f56e6e0c7b6b9f9fd6
eocow <70074743+eocow@users.noreply.github.com>
2024-03-31 16:04:55 -0600

    Create GameSolutionTest.java

---
+++ src/tests/GameSolutionTest.java
@@ -0,0 +1,11 @@
+package tests;
+
+import static org.junit.Assert.*;
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class GameSolutionTest {
+
+}
