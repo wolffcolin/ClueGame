945831f08d986f95ec54cdcdfe8f33f3d4f65640
wolffcolin <cwcnsa@gmail.com>
2024-03-31 16:05:27 -0600

    added computer tests

---
+++ src/tests/ComputerAITest.java
@@ -0,0 +1,18 @@
+package tests;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ComputerAITest {
+
+	@Test
+	void testCreateSuggestion() {
+		
+	}
+	
+	@Test
+	void testSelectTargets() {
+		
+	}
+
+}
