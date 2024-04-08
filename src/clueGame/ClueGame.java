2e70b60f694612e47990ded6d5df975bbdb2f592
wolffcolin <cwcnsa@gmail.com>
2024-04-07 18:35:25 -0600

    added call to board in ClueGame

--- src/clueGame/ClueGame.java
+++ src/clueGame/ClueGame.java
@@ -1,18 +1,25 @@
 package clueGame;
 
 import javax.swing.JFrame;
+import java.awt.Graphics;
 
 public class ClueGame extends JFrame {
+	
+	private static Board board = Board.getInstance();
+	private static Graphics g;
 
 	public ClueGame() {
 		setSize(300, 300);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		board.setConfigFiles("ClueLayout.csv", "ClueSetup.txt");		 
+		board.initialize();
 	}
 
 	public static void main(String[] args) {
-		ClueGame panel = new ClueGame(); // create the panel
-		JFrame frame = new JFrame(); // create the frame
-		frame.setContentPane(panel); // put the panel in the frame
+		ClueGame frame = new ClueGame(); // create the panel
+		g = board.getGraphics();
+		board.paintComponents(g);
+		frame.setContentPane(board); // put the panel in the frame
 		frame.setSize(300, 300); // size the frame
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // allow it to close
 		frame.setTitle("Clue Game Board");
