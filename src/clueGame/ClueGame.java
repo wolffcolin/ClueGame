dc480917aa8dfd9eb40630265675a4a150c80f2d
eocow <70074743+eocow@users.noreply.github.com>
2024-04-07 18:02:08 -0600

    added main function

--- src/clueGame/ClueGame.java
+++ src/clueGame/ClueGame.java
@@ -3,11 +3,21 @@ package clueGame;
 import javax.swing.JFrame;
 
 public class ClueGame extends JFrame {
-	
+
 	public ClueGame() {
 		setSize(300, 300);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 	}
-	
+
+	public static void main(String[] args) {
+		ClueGame panel = new ClueGame(); // create the panel
+		JFrame frame = new JFrame(); // create the frame
+		frame.setContentPane(panel); // put the panel in the frame
+		frame.setSize(300, 300); // size the frame
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // allow it to close
+		frame.setTitle("Clue Game Board");
+		frame.setVisible(true); // make it visible
+	}
+
 }
